1bd743b33f0e6b216d8157e63fd3348f44821d7b
赖守美 <441974767@qq.com>
2022-07-11 11:18:58 +0800

    fix: 配置代理

--- config/proxy.ts
+++ config/proxy.ts
@@ -12,8 +12,9 @@ export default {
     '/api/': {
       // 要代理的地址
       target: 'http://192.168.10.120:5050', // 测试
-      // target: 'http://192.168.10.83:8080',
-      // target: 'http://192.168.10.136:5050/',
+      // target: 'http://192.168.10.83:8085',
+      // target: 'http://192.168.10.101:5050', // 程
+      // target: 'http://192.168.10.136:5050/', // 瞿
       // 配置了这个可以从 http 代理到 https
       // 依赖 origin 的功能可能需要这个，比如 cookie
       changeOrigin: true,
