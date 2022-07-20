bf36588b620a7a52f34509232e35e5e261f97d35
赖守美 <441974767@qq.com>
2022-07-20 14:30:50 +0800

    feat: 通用组件

---
+++ src/components/Input/InputNumber/index.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react'
+import { InputNumber } from 'antd'
+import { numToThousandReg, thousandToNumReg } from '@/utils/reg'
+
+interface inputProps {
+  value?: any
+  onChange?: (vals: any) => void
+  unit?: number // 单位 1万元 2亿元 无元
+}
+
+const ComInputNumber: React.FC<inputProps> = ({ value, onChange = () => {}, unit }) => {
+  const [val, setVal] = useState<number | string>(value)
+  useEffect(() => {
+    if (value && unit) {
+      let newVal = value / 10000
+      if (unit === 1) {
+      } else if (unit === 2) {
+        newVal = value / 100000000
+      }
+      setVal(newVal)
+      onChange(newVal)
+    }
+  }, [unit])
+
+  return (
+    <InputNumber
+      value={val}
+      maxLength={11}
+      min={0}
+      formatter={(vals: any) => numToThousandReg(vals)}
+      parser={(vals: any) => thousandToNumReg(vals)}
+      onChange={(vals) => {
+        console.log(vals)
+        setVal(vals)
+        onChange(vals)
+      }}
+      style={{ width: '100%' }}
+    />
+  )
+}
+
+export default ComInputNumber
