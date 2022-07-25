6fa01021bbdc4913662ab427bf9a15e0c69fbb9e
赖守美 <441974767@qq.com>
2022-07-25 16:46:06 +0800

    feat: 通用标题

--- src/components/ComPage/ComCard/index.tsx
+++ src/components/ComPage/ComCard/index.tsx
@@ -3,13 +3,14 @@ import { Card } from 'antd'
 
 interface comcardprops {
   title: string
+  style?: React.CSSProperties
 }
 
 const ComCard: React.FC<comcardprops> = (props: any) => {
-  const { title } = props
+  const { title, style } = props
 
   return (
-    <Card title={title} bordered={false} style={{ marginBottom: 12 }}>
+    <Card title={title} bordered={false} style={{ marginBottom: 12, ...style }}>
       {props.children}
     </Card>
   )
