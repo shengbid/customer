0bab9264b7116a2d19086cb385f703db91a5626d
赖守美 <441974767@qq.com>
2022-08-09 16:48:43 +0800

    feat: 通过组件封装

---
+++ src/components/ComPage/ComPagContanier/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { PageContainer } from '@ant-design/pro-layout'
+
+interface pageProps {
+  title: string | React.ReactNode
+  extra?: React.ReactNode
+  tabList?: any[]
+  onTabClick?: (key: string) => void
+}
+
+const ComPageContanier: React.FC<pageProps> = (props: any) => {
+  const { tabList = [], title, extra, onTabClick } = props
+  return (
+    <div
+      style={{
+        background: '#F5F7FA',
+      }}
+    >
+      <PageContainer
+        header={{
+          title,
+          breadcrumb: {},
+          ghost: true,
+          extra,
+        }}
+        tabList={tabList}
+        tabProps={{
+          onTabClick,
+        }}
+      >
+        {props.children}
+      </PageContainer>
+    </div>
+  )
+}
+
+export default ComPageContanier
