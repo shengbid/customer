298f5a0f2fe0a9aae8f87b9c8871138d8c820f5a
赖守美 <441974767@qq.com>
2022-08-20 18:44:02 +0800

    feat: 合作企业增加组件

--- src/pages/customerManage/loan/components/CooperateClient/index.tsx
+++ src/pages/customerManage/loan/components/CooperateClient/index.tsx
@@ -1,17 +1,71 @@
 import React, { useState } from 'react'
 import Logistics from './logistics'
-
+import ComEditTable from '@/components/ComProtable/ComEditTable'
+import RequiredLabel from '@/components/RequiredLabel'
+import ComCard from '@/components/ComPage/ComCard'
+import DictSelect from '@/components/ComSelect'
+import { Form } from 'antd'
 interface infoProps {
   enterpriseId: string
 }
 
 const CooperateClient: React.FC<infoProps> = ({ enterpriseId }) => {
-  const [tableData] = useState<any[]>([])
+  const [tableData] = useState<any[]>()
+  const [editableKeys, setEditableRowKeys] = useState<React.Key[]>([])
+  const [dataSource, setDataSource] = useState<any[]>()
+
+  const [tableForm] = Form.useForm()
+
+  const columns = [
+    {
+      title: <RequiredLabel label="仓库名称" />,
+      dataIndex: 'barCode',
+      formItemProps: {
+        rules: [
+          {
+            required: true,
+            message: '此项是必填项',
+          },
+        ],
+      },
+      renderFormItem: () => <DictSelect authorword="warehouse_type" />,
+    },
+    {
+      title: <RequiredLabel label="所属仓库企业" />,
+      dataIndex: 'barCode',
+      editable: false,
+    },
+  ]
 
   return (
     <>
       <Logistics infoData={tableData} enterpriseId={enterpriseId} title="物流" />
       <Logistics infoData={tableData} enterpriseId={enterpriseId} title="仓储" />
+      <ComCard title="合作仓库">
+        <ComEditTable<any>
+          rowKey="key"
+          columns={columns}
+          value={dataSource}
+          recordCreatorProps={{
+            newRecordType: 'dataSource',
+            record: () => ({
+              key: Date.now(),
+            }),
+          }}
+          editable={{
+            type: 'multiple',
+            form: tableForm,
+            editableKeys,
+            onValuesChange: (record: any, recordList: any) => {
+              setDataSource(recordList)
+            },
+            onChange: (editableKeyss: any, editableRows: any[]) => {
+              setEditableRowKeys(editableKeyss)
+              setDataSource(editableRows)
+            },
+          }}
+        />
+      </ComCard>
     </>
   )
 }
